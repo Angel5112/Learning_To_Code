a31e51ba3f0b2ad8da79154bfd06a565f54df33e
angel5112 <aepatino.360@gmail.com>
2023-07-26 18:57:09 -0400

    -Add age_calculator of 001 Ejercicios Basicos

---
+++ Python/Guias/001_Ejercicios_Basicos/age_calculator.py
@@ -0,0 +1,35 @@
+"""
+Ejercicio 4:
+
+Calcule la edad de una persona.
+"""
+
+
+def age_calculator() -> None:
+
+    # Today's date
+
+    YEAR = 2023
+    MONTH = 7
+    DAY = 26
+
+    # Getting the birthdate
+
+    birth_year = int(input("Enter your birth year: "))
+    birth_month = int(input("Enter your birth month: "))
+    birth_day = int(input("Enter your birth day: "))
+
+    # Calculate the age
+
+    age = YEAR - birth_year - 1
+
+    if MONTH > birth_month:
+        age += 1
+    elif MONTH >= birth_month and DAY >= birth_day:
+        age += 1
+
+    print(f"The user is {age} years old!")
+
+
+if __name__ == '__main__':
+    age_calculator()
